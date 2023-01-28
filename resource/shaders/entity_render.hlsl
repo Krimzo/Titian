b6d4d94b0b6ec5f8fa1572b527a38a6b16b727c8
Krimzo <46162167+Krimzo@users.noreply.github.com>
2023-01-28 16:51:01 +0100

    Fixed shader normals

--- resource/shaders/entity_render.hlsl
+++ resource/shaders/entity_render.hlsl
@@ -17,10 +17,10 @@ struct vs_out
 vs_out v_shader(float3 position : KL_Position, float2 textur : KL_Texture, float3 normal : KL_Normal)
 {
     vs_out data;
-    data.world = mul(float4(position, 1.0f), w_matrix).xyz;
-    data.screen = mul(float4(data.world, 1.0f), vp_matrix);
+    data.world = mul(float4(position, 1), w_matrix).xyz;
+    data.screen = mul(float4(data.world, 1), vp_matrix);
     data.textur = textur;
-    data.normal = mul(float4(normal, 0.0f), w_matrix).xyz;
+    data.normal = mul(float4(normal, 0), w_matrix).xyz;
     return data;
 }
 
@@ -35,16 +35,26 @@ cbuffer PS_CB : register(b0)
 SamplerState texture_sampler : register(s0);
 Texture2D entity_texture : register(t0);
 
+float3 get_object_base_color(vs_out vs_data);
+
 float4 p_shader(vs_out vs_data) : SV_Target
 {
-    float3 pixel = object_color;
+    // Light calculations
+    vs_data.normal = normalize(vs_data.normal);
+    const float light_intensity = max(dot(-sun_direction.xyz, vs_data.normal), 0);
 
+    // Color calculations
+    const float3 base_color = get_object_base_color(vs_data);
+    const float3 pixel_color = base_color * light_intensity;
+
+    // Final
+    return float4(pixel_color, 1);
+}
+
+float3 get_object_base_color(vs_out vs_data)
+{
     if (ps_misc_data.x) {
-        pixel = entity_texture.Sample(texture_sampler, vs_data.textur);
+        return entity_texture.Sample(texture_sampler, vs_data.textur);
     }
-
-    const float light_intensity = max(dot(-sun_direction.xyz, vs_data.normal), 0.0f);
-    pixel *= light_intensity;
-    
-    return float4(pixel, 1.0f);
+    return object_color;
 }
