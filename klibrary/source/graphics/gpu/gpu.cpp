09f568f585026e1e30111559d071b254447790a8
Ivan P. <46162167+Krimzo@users.noreply.github.com>
2024-06-12 10:20:32 +0200

    Allowed tearing

--- klibrary/source/graphics/gpu/gpu.cpp
+++ klibrary/source/graphics/gpu/gpu.cpp
@@ -46,7 +46,7 @@ kl::GPU::GPU(const HWND window, const bool debug, const bool single_threaded)
     chain_descriptor.SampleDesc.Count = 1;
     chain_descriptor.Windowed = true;
     chain_descriptor.SwapEffect = DXGI_SWAP_EFFECT_FLIP_DISCARD;
-    chain_descriptor.Flags = DXGI_SWAP_CHAIN_FLAG_ALLOW_MODE_SWITCH;
+    chain_descriptor.Flags = DXGI_SWAP_CHAIN_FLAG_ALLOW_MODE_SWITCH | DXGI_SWAP_CHAIN_FLAG_ALLOW_TEARING;
 
     UINT creation_flags = D3D11_CREATE_DEVICE_BGRA_SUPPORT;
     if (debug) {
@@ -127,7 +127,12 @@ kl::dx::Texture kl::GPU::back_buffer() const
 
 void kl::GPU::swap_buffers(const bool v_sync) const
 {
-    m_chain->Present(v_sync, NULL) >> verify_result;
+    if (v_sync) {
+        m_chain->Present(1, NULL) >> verify_result;
+    }
+    else {
+        m_chain->Present(0, DXGI_PRESENT_ALLOW_TEARING) >> verify_result;
+    }
     bind_internal_views();
 }
 
@@ -175,7 +180,7 @@ void kl::GPU::resize_internal(const Int2& size, const DXGI_FORMAT depth_format)
     for (auto& target : m_d2d1_targets) {
         target.Reset();
     }
-    m_chain->ResizeBuffers(0, 0, 0, DXGI_FORMAT_UNKNOWN, NULL) >> verify_result;
+    m_chain->ResizeBuffers(0, 0, 0, DXGI_FORMAT_UNKNOWN, DXGI_SWAP_CHAIN_FLAG_ALLOW_TEARING) >> verify_result;
 
     // Target buffers
     for (int i = 0; i < BUFFER_COUNT; i++) {
