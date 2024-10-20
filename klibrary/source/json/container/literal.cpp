256517566872147fce0c6a2f4d402aaf33e343fe
Ivan P. <46162167+Krimzo@users.noreply.github.com>
2024-10-20 15:16:54 +0200

    Improved JSON

--- klibrary/source/json/container/literal.cpp
+++ klibrary/source/json/container/literal.cpp
@@ -12,39 +12,41 @@ kl::json::Literal::Literal(const std::string_view& data)
 
 bool kl::json::Literal::compile(std::vector<Token>::const_iterator first, std::vector<Token>::const_iterator last)
 {
-    if (first == last) {
+    if (first == last)
         return false;
-    }
-    const auto& token = *first;
-    if (token.type == TokenType::_NULL) {
-        put_null();
-        return true;
-    }
-    if (token.type == TokenType::_FALSE) {
+
+    switch (first->type)
+    {
+    case TokenType::VAL_NULL:
+		put_null();
+		return true;
+
+	case TokenType::VAL_FALSE:
 		put_bool(false);
-        return true;
-    }
-    if (token.type == TokenType::_TRUE) {
+		return true;
+
+	case TokenType::VAL_TRUE:
 		put_bool(true);
-        return true;
-    }
-    if (token.type == TokenType::_NUMBER) {
-        if (auto opt = parse_float(token.value)) {
-            put_number(opt.value());
-            return true;
-        }
-    }
-    if (token.type == TokenType::_STRING) {
-		put_string(token.value);
-        return true;
-    }
+		return true;
+
+	case TokenType::LIT_NUMBER:
+		if (auto opt = parse_float(first->value)) {
+			put_number(opt.value());
+			return true;
+		}
+        break;
+
+	case TokenType::LIT_STRING:
+		put_string(first->value);
+		return true;
+	}
     return false;
 }
 
 std::string kl::json::Literal::decompile(const int depth) const
 {
     if (auto opt = get_bool()) {
-        return opt.value() ? Standard::true_value : Standard::false_value;
+        return std::string{ opt.value() ? Standard::true_val : Standard::false_val };
     }
 	if (auto opt = get_double()) {
         const double flt_value = opt.value();
@@ -56,9 +58,9 @@ std::string kl::json::Literal::decompile(const int depth) const
 	}
     if (auto opt = get_string()) {
         Lexer::from_escaping(opt.value());
-		return format(Standard::string_literal, opt.value(), Standard::string_literal);
+		return format(Standard::string, opt.value(), Standard::string);
     }
-    return Standard::null_value;
+    return std::string{ Standard::null_val };
 }
 
 void kl::json::Literal::put_null()
